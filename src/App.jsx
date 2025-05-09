4f380811b8649145c134a63aebd70544a97d6d9d
BothSann <boththann76@gmail.com>
2025-05-09 16:10:26 +0700

    Implement city and country lists with data fetching; add CityItem and CityList components; enhance AppNav for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,51 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+      } catch (err) {
+        console.alert(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCities();
+
+    return () => {};
+  }, []);
+
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route path="cities" element={<p>List of cities</p>} />
-          <Route path="countries" element={<p>City by country</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<CountryList />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,7 @@
+import styles from "./CountryList.module.css";
+
+function CountryList() {
+  return <div className={styles.countryList}>CountryList</div>;
+}
+
+export default CountryList;
