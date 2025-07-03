8bb2fcc7ff0c80c5f6a9ab0c2b22dbd9b4d11826
BothSann <boththann76@gmail.com>
2025-07-03 17:40:48 +0700

    Add authentication context and user management features
    - Introduced FakeAuthContext for managing user authentication state.
    - Updated App component to include AuthProvider for authentication context.
    - Modified User component to utilize authentication context for user data and logout functionality.
    - Enhanced Login component to handle user login and redirect upon successful authentication.
    - Added .env file for demo email and password configuration.
    - Integrated User component into AppLayout for displaying user information.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,35 @@
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import Button from "../components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
+  const [email, setEmail] = useState("qwerty@example.com");
   const [password, setPassword] = useState("qwerty");
+  const navigate = useNavigate();
+
+  const { login, isAuthenticated } = useAuth();
+
+  function handleSubmit(e) {
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    e.preventDefault();
+    if (email && password) login(email, password);
+    else alert("Provide email and password");
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/app", { replace: true });
+  }, [isAuthenticated, navigate]);
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +51,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
